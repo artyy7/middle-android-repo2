1b6fa3b9f28221d87402b1e86f56da75e5bae691
Artemka <artemka7796@gmail.com>
2026-07-05 18:02:58 +0300

    task 2 completed

--- app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
+++ app/src/main/java/ru/yandex/praktikumchatapp/data/ChatRepository.kt
@@ -1,12 +1,30 @@
 package ru.yandex.praktikumchatapp.data
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retryWhen
 
 class ChatRepository(
     private val api: ChatApi = ChatApi()
 ) {
 
     fun getReplyMessage(): Flow<String> {
-        return api.getReply()  // TODO Задание 2: добавьте обработку ошибок
+        var delayMs = RETRY_DELAY_MS
+
+        return api.getReply().retryWhen { cause, attempt ->
+            if (cause is Exception && attempt < MAX_RETRIES) {
+                delay(delayMs)
+                delayMs *= 2
+                return@retryWhen true
+            }
+
+            return@retryWhen false
+        }
+    }
+
+
+    companion object {
+        const val RETRY_DELAY_MS = 500L
+        const val MAX_RETRIES = 5
     }
 }
